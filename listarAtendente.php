1e6304aa536bd515d12be03e093d5c574c9a0509
MarcoAntonioNobre <0001000610@senaimgaluno.com.br>
2024-04-03 13:40:32 -0300

    Tabelas

--- listarAtendente.php
+++ listarAtendente.php
@@ -1,2 +1,40 @@
 <?php
 echo 'atende';
+?>
+
+<?php
+echo 'cliente';
+
+?>
+
+<div class="card">
+    <div class="card-header fs-4">
+        <i class="bi bi-person"></i> Atendentes
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>João Paulo</td>
+                <td>joao@gmail.com</td>
+                <td>
+                    <Button>Alterar</Button>
+                    <Button>Alterar</Button>
+                    <Button>Alterar</Button>
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
