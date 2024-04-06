3861e9b284dcfeed14491b96a31dcd4fb34d54c7
MarcoAntonioNobre <0001000610@senaimgaluno.com.br>
2024-04-06 11:39:35 -0300

    Telinha de loading

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ include_once('navbar.php');
                         <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z"></path>
                     </g>
                 </svg>
-                <input type="email" class="input" placeholder="Email" id="email">
+                <input type="email" class="input" placeholder="Email" id="email" autofocus>
             </div>
 
             <div class="flex-column fonte">
@@ -96,6 +96,10 @@ include_once('navbar.php');
 
 </div>
 
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<script src="/node_modules/@lottiefiles/lottie-player/dist/lottie-player.js"></script>
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/tgs-player.js"></script>
+<script src="/node_modules/@lottiefiles/lottie-player/dist/tgs-player.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
         crossorigin="anonymous"></script>
